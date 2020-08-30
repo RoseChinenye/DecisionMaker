a3c02cfd7bf955b563056979a0fff97c32034b00
RoseChinenye <60973945+RoseChinenye@users.noreply.github.com>
2020-08-30 23:11:01 +0100

    Add files via upload

---
+++ build/precache-manifest.b5651c1fa02aedf2fb7a6f3b574440e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd459c854a0eb84a8764f08dd2482253",
+    "url": "/index.html"
+  },
+  {
+    "revision": "584238d235e78c96b0bc",
+    "url": "/static/css/main.ad573f5c.chunk.css"
+  },
+  {
+    "revision": "67a6b61e3c815ed25f6c",
+    "url": "/static/js/2.180d86fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.180d86fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "584238d235e78c96b0bc",
+    "url": "/static/js/main.58d499ec.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
